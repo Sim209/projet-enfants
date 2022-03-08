aaca487208eb98aed3b1a438ade12fd0763150f6
Sim209 <Sim209@users.noreply.github.com>
2022-03-08 17:14:14 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,14 @@ function Eteint_les_LEDs () {
     moveMotorZIP.clear()
     moveMotorZIP.show()
 }
+//% color="#ff6800" icon="\uf1b9" weight=15
+//% groups="['Lampes Voiture']"
+    /**
+     * car run diretion
+     */
+    //% block="Allume les leds  $direction à la vitesse de $speed \\%"
+    //% speed.min=0 speed.max=100
+    //% group="Lampes Voiture" weight=99
 function Allume_les_LEDs (Couleur: string) {
     moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
     if (Couleur == "rouge") {
